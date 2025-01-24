4fce5402cc22be705bf332c385677c0084af4923
nfoert <nofoert@gmail.com>
2025-01-24 16:56:33 -0500

    feat: Add max-width to image

--- styles.py
+++ styles.py
@@ -2,7 +2,7 @@ styles = {
     "body_content": "border: 1px solid black; padding: 10px; margin: 10px; border-radius: 20px; background-color: rgb(255, 255, 255); margin-top: 20px; margin-bottom: 20px;",
     "body": "background-color: rgba(0, 0, 50, 0.08); margin: 0px; padding: 0px;",
     "p": "font-family: 'Inter', 'Noto Sans', Arial, sans-serif; font-size: 16px;",
-    "img": "width: 80vw; height: auto; margin-left: auto; margin-right: auto; display: block;",
+    "img": "width: 80vw; height: auto; margin-left: auto; margin-right: auto; display: block; max-width: 600px;",
     "h1": "font-family: 'Inter', 'Noto Sans', Arial, sans-serif; font-size: 32px;",
     "h2": "font-family: 'Inter', 'Noto Sans', Arial, sans-serif; font-size: 24px;",
     "h3": "font-family: 'Inter', 'Noto Sans', Arial, sans-serif; font-size: 20px;",
